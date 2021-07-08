c19e8b42ead4aa18748ea7293180942ee1622231
Dmitrii <Pipeladze1@gmail.com>
2021-07-08 22:41:06 +0500

    Button styles 1

--- UITemplate/MainWindow.xaml.cs
+++ UITemplate/MainWindow.xaml.cs
@@ -25,9 +25,32 @@ namespace UITemplate
             InitializeComponent();
         }
 
+        private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DragWindowBorder_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Maximize_NormalWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+
+        private void MinimizeWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
         private void MainGrid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            OpenCloseMenuToggleButton.IsChecked = false;
+            //OpenCloseMenuToggleButton.IsChecked = false;
         }
     }
 }
